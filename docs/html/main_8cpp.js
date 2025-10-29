5c0d4c9f4db58002630f8d5fa806ea6b8b1cf847
trionios <rubencasann@gmail.com>
2025-10-29 08:52:45 +0100

    docs: generar documentación inicial con Doxygen

---
+++ docs/html/main_8cpp.js
@@ -0,0 +1,5 @@
+var main_8cpp =
+[
+    [ "main", "main_8cpp.html#ae66f6b31b5ad750f1fe042a706a4e3d4", null ],
+    [ "sumar", "main_8cpp.html#ad2fc341d60a4e2d9598d41b04d4e513a", null ]
+];
---
+++ docs/html/search/all_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['sumar_0',['sumar',['../main_8cpp.html#ad2fc341d60a4e2d9598d41b04d4e513a',1,'main.cpp']]]
+];
